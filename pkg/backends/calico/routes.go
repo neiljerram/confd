72389d168335a68b0cd90cfaa2c23d5e3d0c53ab
Neil Jerram <neil@tigera.io>
2018-11-02 17:33:00 +0000

    wip - dynamic advertisement

--- pkg/backends/calico/routes.go
+++ pkg/backends/calico/routes.go
@@ -14,8 +14,12 @@
 package calico
 
 import (
+	"fmt"
 	"os"
 	"strings"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 type routeGenerator struct {
@@ -32,17 +36,45 @@ func NewRouteGenerator(c *client, kubeconfig string) (rg *routeGenerator, err er
 }
 
 func (rg *routeGenerator) Start() (err error) {
-	// MVP implementation: read CIDRs to advertise,
-	// comma-separated, from an environment variable
-	// CALICO_STATIC_ROUTES.
-	routeString := os.Getenv("CALICO_STATIC_ROUTES")
-	cidrs := []string{}
-	for _, route := range strings.Split(routeString, ",") {
-		cidr := strings.TrimSpace(route)
-		if cidr != "" {
-			cidrs = append(cidrs, cidr)
+	if rg.kubeconfig != "" {
+		// Dynamic implementation based on currently active k8s Services and Endpoints.
+		k8sClientset, err := getClients(rg.kubeconfig)
+		if err != nil {
+			log.WithError(err).Error("Failed to create k8s clientset")
+			return err
 		}
+	} else {
+		// MVP implementation: read CIDRs to advertise,
+		// comma-separated, from an environment variable
+		// CALICO_STATIC_ROUTES.
+		routeString := os.Getenv("CALICO_STATIC_ROUTES")
+		cidrs := []string{}
+		for _, route := range strings.Split(routeString, ",") {
+			cidr := strings.TrimSpace(route)
+			if cidr != "" {
+				cidrs = append(cidrs, cidr)
+			}
+		}
+		rg.client.updateRoutes(cidrs)
 	}
-	rg.client.updateRoutes(cidrs)
 	return
+}
+
+// Stolen from kube-controllers: getClients builds and returns Kubernetes clients.
+func getClients(kubeconfig string) (*kubernetes.Clientset, error) {
+
+	// Build Kubernetes config; we support in-cluster config and kubeconfig as means of
+	// configuring the client.
+	k8sconfig, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build kubernetes client config: %s", err)
+	}
+
+	// Get Kubernetes clientset.
+	k8sClientset, err := kubernetes.NewForConfig(k8sconfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build kubernetes client: %s", err)
+	}
+
+	return k8sClientset, nil
 }
